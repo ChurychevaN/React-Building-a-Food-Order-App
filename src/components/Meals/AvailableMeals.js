9e58e42b9dd884f3e2f36ed8fb18d9660918b630
Your Name <you@example.com>
2022-07-13 14:38:32 +0200

    Adding-A-CheckoutForm

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -11,7 +11,7 @@ const AvailableMeals = () => {
 	useEffect(() => {
 		const fetchMeals = async () => {
 			const response = await fetch(
-				'https://react-http-162e2-default-rtdb.firebaseio.com/meals.json'); //.json
+				'https://react-http-162e2-default-rtdb.firebaseio.com/meals.json'); //!.json
 
 			if (!response.ok) {
 				throw new Error('Something went wrong!');
